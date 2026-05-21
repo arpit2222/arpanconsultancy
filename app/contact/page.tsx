1d4da9723f1db11a73f0f5fc951993239f4c1bee
spare93mail-source <spare93mail@gmail.com>
2026-05-21 05:49:25 +0530

    Update contact information in page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -20,9 +20,9 @@ export default function ContactPage() {
             <article className="card">
               <h3>General inquiries</h3>
               <div className="contact-list">
-                <a href="tel:+16475287458">(647) 528-7458</a>
-                <a href="tel:+14273727296">(427) 372-7296</a>
-                <a href="mailto:consulting@mail.com">consulting@mail.com</a>
+                <a href="tel:+918178845901">+91 81788 45901</a>
+                <a href="tel:+918178845901">(427) 372-7296</a>
+                <a href="mailto:chauhanarpan88@gmail.com">chauhanarpan88@gmail.com</a>
               </div>
             </article>
             <article className="card">
